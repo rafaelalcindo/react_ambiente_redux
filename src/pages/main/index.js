5496a4a5d055382ea89b60537f1374d3a4d360fe
Rafael Alcindo <rafael_alcindo@hotmail.com>
2020-10-20 23:02:21 -0300

    adding footer and redux in footer

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,67 @@
 import React, { Component, Fragment } from 'react'
+import PropTypes from 'prop-types'
 
-const Main = () => <h1>Main</h1>
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as FavoriteActions from '../../store/actions/favorites'
 
-export default Main;
+class Main extends Component {
+
+  static propTypes = {
+    addFavorite: PropTypes.func.isRequired,
+    favorites: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      description: PropTypes.string,
+      url: PropTypes.string
+    })).isRequired
+  }
+
+  state = {
+    repositoryInput: '',
+  }
+
+  handleAddRepository = (event) => {
+    event.preventDefault();
+
+    this.props.addFavorite();
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <form onSubmit={this.handleAddRepository} >
+          <input
+            placeholder="usuário/repositório"
+            value={this.state.repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit" >Adicionar</button>
+
+          <ul>
+            {this.props.favorites.map(favorite => (
+              <li key={favorite.id} >
+                <p>
+                  <strong>{favorite.name}</strong> ({favorite.description})
+                </p>
+                <a href={favorite.url} >Acessar</a>
+              </li>
+            ))}
+          </ul>
+        </form>
+      </Fragment>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  favorites: state.favorites
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators(FavoriteActions, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
+
+// const Main = () => <h1>Main</h1>
+
+// export default Main;
